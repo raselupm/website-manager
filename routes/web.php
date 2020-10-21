7be1682cf8a8e50ce0a85e88b170d69eb4c0eb15
Ahmed Shamim <shaon.cse81@gmail.com>
2020-10-21 10:47:44 +0600

    refactor: clean up route by grouping under same middleware

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,24 @@ use App\Http\Controllers\ServerController;
 |
 */
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/', [DomainController::class, 'index'])->name('dashboard');
-Route::middleware(['auth:sanctum', 'verified'])->get('/servers', [ServerController::class, 'index'])->name('servers');
-Route::middleware(['auth:sanctum', 'verified'])->get('/{name}', [DomainController::class, 'show'])->name('domain-single');
-Route::middleware(['auth:sanctum', 'verified'])->get('/edit-server/{id}', [ServerController::class, 'edit'])->name('edit-server');
-Route::middleware(['auth:sanctum', 'verified'])->put('/edit-server/{id}', [ServerController::class, 'update'])->name('update-server');
-Route::middleware(['auth:sanctum', 'verified'])->post('/delete-server/{id}', [ServerController::class, 'destroy'])->name('delete-server');
-Route::middleware(['auth:sanctum', 'verified'])->post('/search', [DomainController::class, 'search'])->name('domain-search');
-Route::middleware(['auth:sanctum', 'verified'])->post('/add-domain', [DomainController::class, 'store'])->name('add');
-Route::middleware(['auth:sanctum', 'verified'])->post('/add-server', [ServerController::class, 'store'])->name('add-server');
-Route::middleware(['auth:sanctum', 'verified'])->post('/refresh', [DomainController::class, 'refresh'])->name('refresh-dns');
+Route::middleware(['auth:sanctum', 'verified'])->group(function() {
+    Route::get('/', [DomainController::class, 'index'])->name('dashboard');
+    Route::get('/servers', [ServerController::class, 'index'])->name('servers');
+    Route::get('/{name}', [DomainController::class, 'show'])->name('domain-single');
+    Route::get('/edit-server/{id}', [ServerController::class, 'edit'])->name('edit-server');
+    Route::put('/edit-server/{id}', [ServerController::class, 'update'])->name('update-server');
+    Route::post('/delete-server/{id}', [ServerController::class, 'destroy'])->name('delete-server');
+    Route::post('/search', [DomainController::class, 'search'])->name('domain-search');
+    Route::post('/add-domain', [DomainController::class, 'store'])->name('add');
+    Route::post('/add-server', [ServerController::class, 'store'])->name('add-server');
+    Route::post('/refresh', [DomainController::class, 'refresh'])->name('refresh-dns');
 
-Route::middleware(['auth:sanctum', 'verified'])->post('/domains/getDomains/', [DomainController::class, 'getDomains'])->name('domains.getDomains');
+    Route::post('/domains/getDomains/', [DomainController::class, 'getDomains'])->name('domains.getDomains');
 
-Route::middleware(['auth:sanctum', 'verified'])->post('/check', [DomainController::class, 'check'])->name('domain-dns-check');
-Route::middleware(['auth:sanctum', 'verified'])->get('/external', [DomainController::class, 'external'])->name('domain-dns-external');
+    Route::post('/check', [DomainController::class, 'check'])->name('domain-dns-check');
+    Route::get('/external', [DomainController::class, 'external'])->name('domain-dns-external');
 
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/edit-domain/{id}', [DomainController::class, 'edit'])->name('edit-domain');
-Route::middleware(['auth:sanctum', 'verified'])->put('/edit-domain/{id}', [DomainController::class, 'update'])->name('update-domain');
-Route::middleware(['auth:sanctum', 'verified'])->post('/delete-domain/{id}', [DomainController::class, 'destroy'])->name('delete-domain');
+    Route::get('/edit-domain/{id}', [DomainController::class, 'edit'])->name('edit-domain');
+    Route::put('/edit-domain/{id}', [DomainController::class, 'update'])->name('update-domain');
+    Route::post('/delete-domain/{id}', [DomainController::class, 'destroy'])->name('delete-domain');
+});
